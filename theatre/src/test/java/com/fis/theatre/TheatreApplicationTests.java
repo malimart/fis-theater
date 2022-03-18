0c749dabe1be86c51bb013e0101f387773bed390
Jovic92 <milan.jovic@dmi.uns.ac.rs>
2022-03-18 16:26:59 +0100

    initial commit

---
+++ theatre/src/test/java/com/fis/theatre/TheatreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fis.theatre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheatreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

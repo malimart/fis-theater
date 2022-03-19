5243eed39b8e49ff10a512b0232508568adbd8e5
Jovic92 <milan.jovic@dmi.uns.ac.rs>
2022-03-19 09:20:29 +0100

    Day 2 done

---
+++ theatre/src/main/resources/data.sql
@@ -0,0 +1,272 @@
+
+INSERT INTO actor VALUES (1,'Ethan','Hawke');
+INSERT INTO actor VALUES (2,'Kyle','MacLachlan');
+INSERT INTO actor VALUES (3,'Diane','Venora');
+INSERT INTO actor VALUES (4,'Sam','Shepard');
+INSERT INTO actor VALUES (5,'Bill','Murray');
+INSERT INTO actor VALUES (6,'Liev','Schreiber');
+INSERT INTO actor VALUES (7,'Julia','Stiles');
+INSERT INTO actor VALUES (8,'Steve','Zahn');
+INSERT INTO actor VALUES (9,'Julie','Andrews');
+INSERT INTO actor VALUES (10,'Dick','Van Dyke');
+INSERT INTO actor VALUES  (11,'Davdi','Tomlinson');
+INSERT INTO actor VALUES (12,'Gylnis','Johns');
+INSERT INTO actor VALUES (13,'Hermione','Baddeley');
+INSERT INTO actor VALUES (14,'Reta','Shaw');
+INSERT INTO actor VALUES (15,'Karen','Dotrice');
+INSERT INTO actor VALUES (16,'Matthew','Gaber');
+INSERT INTO actor VALUES (17,'Gregory','Peck');
+INSERT INTO actor VALUES (18,'John','Megna');
+INSERT INTO actor VALUES (19,'Frank','Overton');
+INSERT INTO actor VALUES (20,'Rosemary','Murphy');
+INSERT INTO actor VALUES (21,'Ruth','White');
+INSERT INTO actor VALUES (22,'Brock','Peters');
+INSERT INTO actor VALUES (23,'Estelle','Evans');
+INSERT INTO actor VALUES (24,'Paul','Fix');
+INSERT INTO actor VALUES (25,'Denzel','Washington');
+INSERT INTO actor VALUES (26,'Frances','McDormand');
+INSERT INTO actor VALUES (27,'Alex','Hassell');
+INSERT INTO actor VALUES (28,'Bertie','Carvel');
+INSERT INTO actor VALUES (29,'Brendan','Gleeson');
+INSERT INTO actor VALUES (30,'Corey','Hawkins');
+INSERT INTO actor VALUES (31,'Harry','Melling');
+INSERT INTO actor VALUES (32,'Miles','Anderson');
+INSERT INTO actor VALUES (33,'Glenn','Ford');
+INSERT INTO actor VALUES (34,'Van','Heflin');
+INSERT INTO actor VALUES (35,'Richard','Jaeckel');
+INSERT INTO actor VALUES (36,'Russell','Crowe');
+INSERT INTO actor VALUES (37,'Chritian','Bale');
+INSERT INTO actor VALUES (38,'Ben','Foster');
+
+INSERT INTO director VALUES (1,'Michael','Almereyda');
+INSERT INTO director VALUES  (2,'Robert','Stevenson');
+INSERT INTO director VALUES  (3,'Robert','Mulligan');
+INSERT INTO director VALUES  (4,'Joel','Coen');
+INSERT INTO director VALUES  (5,'Delmer','Davis');
+
+
+INSERT INTO genre VALUES (1,'Action');
+INSERT INTO genre VALUES  (2,'Crime');
+INSERT INTO genre VALUES  (3,'Drama');
+INSERT INTO genre VALUES  (4,'Thriller');
+INSERT INTO genre VALUES  (5,'Western');
+INSERT INTO genre VALUES  (6,'History');
+INSERT INTO genre VALUES  (7,'Comedy');
+INSERT INTO genre VALUES  (8,'Family');
+INSERT INTO genre VALUES  (9,'Fantasy');
+INSERT INTO genre VALUES  (10,'Romance');
+
+INSERT INTO show_theatre VALUES (1,'Modern-day New York City adaptation of Shakespeares immortal story about Hamlets plight to avenge his fathers murder.',120,'Hamlet',1);
+INSERT INTO show_theatre VALUES (2,'In turn of the century London, a magical nanny employs music and adventure to help two neglected children become closer to their father.',130,'Marry Poppins',2);
+INSERT INTO show_theatre VALUES (3,'Atticus Finch, a widowed lawyer in Depression-era Alabama, defends a black man against an undeserved rape charge, and his children against prejudice.',129,'To Kill a Mocking Bird',3);
+INSERT INTO show_theatre VALUES (4,'A Scottish lord becomes convinced by a trio of witches that he will become the next King of Scotland, and his ambitious wife supports him in his plans of seizing power.',105,'The Tradgedy of Macbeth',4);
+INSERT INTO show_theatre VALUES (5,'Broke small-time rancher Dan Evans is hired by the stagecoach line to put big-time captured outlaw leader Ben Wade on the 3:10 train to Yuma but Wades gang tries to free him.',92,'3:10 to Yuma',5);
+
+
+
+INSERT INTO role VALUES (1,'Hamlet',1);
+INSERT INTO role VALUES  (2,'Cludius',1);
+INSERT INTO role VALUES  (3,'Getrude',1);
+INSERT INTO role VALUES  (4,'Ghost',1);
+INSERT INTO role VALUES  (5,'Polonius',1);
+INSERT INTO role VALUES  (6,'Laertes',1);
+INSERT INTO role VALUES  (7,'Ophelia',1);
+INSERT INTO role VALUES  (8,'Horatio',1);
+INSERT INTO role VALUES  (9,'Marry Poppins',2);
+INSERT INTO role VALUES  (10,'Bert',2);
+INSERT INTO role VALUES  (11,'George',2);
+INSERT INTO role VALUES  (12,'Wubbufred',2);
+INSERT INTO role VALUES  (13,'Ellen',2);
+INSERT INTO role VALUES  (14,'Brill-Cook',2);
+INSERT INTO role VALUES  (15,'Jane',2);
+INSERT INTO role VALUES  (16,'Michael',2);
+INSERT INTO role VALUES  (17,'Atticus',3);
+INSERT INTO role VALUES  (18,'Dill',3);
+INSERT INTO role VALUES  (19,'Sheriff tate',3);
+INSERT INTO role VALUES  (20,'Muadie',3);
+INSERT INTO role VALUES  (21,'Dubose',3);
+INSERT INTO role VALUES  (22,'Tom',3);
+INSERT INTO role VALUES  (23,'Calpurnia',3);
+INSERT INTO role VALUES  (24,'Judge Taylor',3);
+INSERT INTO role VALUES  (25,'Macbeth',4);
+INSERT INTO role VALUES  (26,'Lady Macbeth',4);
+INSERT INTO role VALUES  (27,'Ross',4);
+INSERT INTO role VALUES  (28,'Banquo',4);
+INSERT INTO role VALUES  (29,'Duncan',4);
+INSERT INTO role VALUES  (30,'Macduff',4);
+INSERT INTO role VALUES  (31,'Malcom',4);
+INSERT INTO role VALUES  (32,'Lennox',4);
+INSERT INTO role VALUES  (33,'Ben Wade',5);
+INSERT INTO role VALUES  (34,'Dan Evans',5);
+INSERT INTO role VALUES  (35,'Charlie Prince',5);
+
+
+INSERT INTO acts VALUES (1,1,1);
+INSERT INTO acts VALUES  (2,2,2);
+INSERT INTO acts VALUES  (3,3,3);
+INSERT INTO acts VALUES  (4,4,4);
+INSERT INTO acts VALUES  (5,5,5);
+INSERT INTO acts VALUES  (6,6,6);
+INSERT INTO acts VALUES  (7,7,7);
+INSERT INTO acts VALUES  (8,8,8);
+INSERT INTO acts VALUES  (9,9,9);
+INSERT INTO acts VALUES  (10,10,10);
+INSERT INTO acts VALUES  (11,11,11);
+INSERT INTO acts VALUES  (12,12,12);
+INSERT INTO acts VALUES  (13,13,13);
+INSERT INTO acts VALUES  (14,14,14);
+INSERT INTO acts VALUES  (15,15,15);
+INSERT INTO acts VALUES  (16,16,16);
+INSERT INTO acts VALUES  (17,17,17);
+INSERT INTO acts VALUES  (18,18,18);
+INSERT INTO acts VALUES  (19,19,19);
+INSERT INTO acts VALUES  (20,20,20);
+INSERT INTO acts VALUES  (21,21,21);
+INSERT INTO acts VALUES  (22,22,22);
+INSERT INTO acts VALUES  (23,23,23);
+INSERT INTO acts VALUES  (24,24,24);
+INSERT INTO acts VALUES  (25,25,25);
+INSERT INTO acts VALUES  (26,26,26);
+INSERT INTO acts VALUES  (27,27,27);
+INSERT INTO acts VALUES  (28,28,28);
+INSERT INTO acts VALUES  (29,29,29);
+INSERT INTO acts VALUES  (30,30,30);
+INSERT INTO acts VALUES  (31,31,31);
+INSERT INTO acts VALUES  (32,32,32);
+INSERT INTO acts VALUES  (33,33,33);
+INSERT INTO acts VALUES  (34,34,34);
+INSERT INTO acts VALUES  (35,35,35);
+INSERT INTO acts VALUES  (36,36,33);
+INSERT INTO acts VALUES  (37,37,34);
+INSERT INTO acts VALUES  (38,38,35);
+
+
+
+
+INSERT INTO visitor VALUES (1,'milan@gmail.com','Milana','Jovic','milan','milan');
+INSERT INTO visitor VALUES (2,'ana@gmail.com','Ana','Jovic','ana','ana');
+
+INSERT INTO scene VALUES (1,'Big scene');
+INSERT INTO scene VALUES  (2,'Small scene');
+INSERT INTO scene VALUES  (3,'Scene in honor off...');
+
+INSERT INTO location VALUES (1,'1',1,1,1);
+INSERT INTO location VALUES  (2,'1',1,2,1);
+INSERT INTO location VALUES  (3,'1',1,3,1);
+INSERT INTO location VALUES  (4,'1',1,1,2);
+INSERT INTO location VALUES  (5,'1',1,2,2);
+INSERT INTO location VALUES  (6,'1',1,3,2);
+INSERT INTO location VALUES  (7,'1',1,1,3);
+INSERT INTO location VALUES  (8,'1',1,2,3);
+INSERT INTO location VALUES  (9,'1',1,3,3);
+INSERT INTO location VALUES  (10,'1',2,1,1);
+INSERT INTO location VALUES  (11,'1',2,2,1);
+INSERT INTO location VALUES  (12,'1',2,3,1);
+INSERT INTO location VALUES  (13,'1',2,1,2);
+INSERT INTO location VALUES  (14,'1',2,2,2);
+INSERT INTO location VALUES  (15,'1',2,3,2);
+INSERT INTO location VALUES  (16,'1',3,1,1);
+INSERT INTO location VALUES  (17,'1',3,1,2);
+INSERT INTO location VALUES  (18,'1',3,1,3);
+
+
+INSERT INTO genre_show VALUES (1,3,1);
+INSERT INTO genre_show VALUES  (2,10,1);
+INSERT INTO genre_show VALUES  (3,4,1);
+INSERT INTO genre_show VALUES  (4,7,2);
+INSERT INTO genre_show VALUES  (5,8,2);
+INSERT INTO genre_show VALUES  (6,9,2);
+INSERT INTO genre_show VALUES  (7,2,3);
+INSERT INTO genre_show VALUES  (8,3,3);
+INSERT INTO genre_show VALUES  (9,3,4);
+INSERT INTO genre_show VALUES  (10,6,4);
+INSERT INTO genre_show VALUES  (11,4,4);
+INSERT INTO genre_show VALUES  (12,3,5);
+INSERT INTO genre_show VALUES  (13,4,5);
+INSERT INTO genre_show VALUES  (14,5,5);
+INSERT INTO genre_show VALUES  (15,1,5);
+INSERT INTO genre_show VALUES  (16,2,5);
+INSERT INTO genre_show VALUES  (17,3,5);
+
+
+
+INSERT INTO performance VALUES (1,'2022-03-18',1,1);
+INSERT INTO performance VALUES  (2,'2022-03-18',2,2);
+INSERT INTO performance VALUES  (3,'2022-03-18',3,3);
+INSERT INTO performance VALUES  (4,'2022-03-19',1,4);
+INSERT INTO performance VALUES  (5,'2022-03-18',2,5);
+INSERT INTO performance VALUES  (6,'2022-03-22',2,1);
+INSERT INTO performance VALUES  (7,'2022-03-22',3,2);
+INSERT INTO performance VALUES  (8,'2022-03-22',1,4);
+INSERT INTO performance VALUES  (9,'2022-03-22',1,5);
+
+INSERT INTO acts_in_performance VALUES (1,1,1);
+INSERT INTO acts_in_performance VALUES  (2,2,1);
+INSERT INTO acts_in_performance VALUES  (3,3,1);
+INSERT INTO acts_in_performance VALUES  (4,4,1);
+INSERT INTO acts_in_performance VALUES  (5,5,1);
+INSERT INTO acts_in_performance VALUES  (6,6,1);
+INSERT INTO acts_in_performance VALUES  (7,7,1);
+INSERT INTO acts_in_performance VALUES  (8,8,1);
+INSERT INTO acts_in_performance VALUES  (9,1,6);
+INSERT INTO acts_in_performance VALUES  (10,2,6);
+INSERT INTO acts_in_performance VALUES  (11,3,6);
+INSERT INTO acts_in_performance VALUES  (12,4,6);
+INSERT INTO acts_in_performance VALUES  (13,5,6);
+INSERT INTO acts_in_performance VALUES  (14,6,6);
+INSERT INTO acts_in_performance VALUES  (15,7,6);
+INSERT INTO acts_in_performance VALUES  (16,8,6);
+INSERT INTO acts_in_performance VALUES  (17,9,2);
+INSERT INTO acts_in_performance VALUES  (18,10,2);
+INSERT INTO acts_in_performance VALUES  (19,11,2);
+INSERT INTO acts_in_performance VALUES  (20,12,2);
+INSERT INTO acts_in_performance VALUES  (21,13,2);
+INSERT INTO acts_in_performance VALUES  (22,14,2);
+INSERT INTO acts_in_performance VALUES  (23,15,2);
+INSERT INTO acts_in_performance VALUES  (24,16,2);
+INSERT INTO acts_in_performance VALUES  (25,9,7);
+INSERT INTO acts_in_performance VALUES  (26,10,7);
+INSERT INTO acts_in_performance VALUES  (27,11,7);
+INSERT INTO acts_in_performance VALUES  (28,12,7);
+INSERT INTO acts_in_performance VALUES  (29,13,7);
+INSERT INTO acts_in_performance VALUES  (30,14,7);
+INSERT INTO acts_in_performance VALUES  (31,15,7);
+INSERT INTO acts_in_performance VALUES  (32,16,7);
+INSERT INTO acts_in_performance VALUES  (33,17,3);
+INSERT INTO acts_in_performance VALUES  (34,18,3);
+INSERT INTO acts_in_performance VALUES  (35,19,3);
+INSERT INTO acts_in_performance VALUES  (36,20,3);
+INSERT INTO acts_in_performance VALUES  (37,21,3);
+INSERT INTO acts_in_performance VALUES  (38,22,3);
+INSERT INTO acts_in_performance VALUES  (39,23,3);
+INSERT INTO acts_in_performance VALUES  (40,24,3);
+INSERT INTO acts_in_performance VALUES  (41,25,4);
+INSERT INTO acts_in_performance VALUES  (42,26,4);
+INSERT INTO acts_in_performance VALUES  (43,27,4);
+INSERT INTO acts_in_performance VALUES  (44,28,4);
+INSERT INTO acts_in_performance VALUES  (45,29,4);
+INSERT INTO acts_in_performance VALUES  (46,30,4);
+INSERT INTO acts_in_performance VALUES  (47,31,4);
+INSERT INTO acts_in_performance VALUES  (48,32,4);
+INSERT INTO acts_in_performance VALUES  (49,25,8);
+INSERT INTO acts_in_performance VALUES  (50,26,8);
+INSERT INTO acts_in_performance VALUES  (51,27,8);
+INSERT INTO acts_in_performance VALUES  (52,28,8);
+INSERT INTO acts_in_performance VALUES  (53,29,8);
+INSERT INTO acts_in_performance VALUES  (54,30,8);
+INSERT INTO acts_in_performance VALUES  (55,31,8);
+INSERT INTO acts_in_performance VALUES  (56,32,8);
+INSERT INTO acts_in_performance VALUES  (57,33,5);
+INSERT INTO acts_in_performance VALUES  (58,34,5);
+INSERT INTO acts_in_performance VALUES  (59,35,5);
+INSERT INTO acts_in_performance VALUES  (60,36,9);
+INSERT INTO acts_in_performance VALUES  (61,37,9);
+INSERT INTO acts_in_performance VALUES  (62,38,9);
+
+
+
+
+
+
+
+
